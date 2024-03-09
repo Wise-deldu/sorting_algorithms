40f3ebceb987f66f7123ce39ed51486302a819b8
Wise-deldu <delduwise@gmail.com>
2024-03-09 21:19:30 +0000

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,10 +3,9 @@
 /**
  * len_list - returns the length of a linked list
  * @h: pointer to the list
- * 
+ *
  * Return: length of list
  */
-
 int len_list(listint_t *h)
 {
 	int len = 0;
@@ -23,12 +22,11 @@ int len_list(listint_t *h)
  * insertion_sort_list - sorts a linked list with the Insert Sort algorithm
  * @list: double pointer to the list to sort
  */
-
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr = NULL, *one = NULL;
 	listint_t *two = NULL, *three = NULL, *four = NULL;
-	
+
 	if (!list || !(*list) || len_list(*list) < 2)
 		return;
 
